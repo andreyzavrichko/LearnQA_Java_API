63120c9aaba1f2bc72c100859ff46bac5dcf7971
Andrey Zavrichko <andrey@zavrichko.dev>
2022-09-17 17:43:01 +0300

    выполнил EX6

--- src/test/java/lessonTwo.java
+++ src/test/java/lessonTwo.java
@@ -1,19 +1,40 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
+
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+
 
 class lessonTwo {
     @Test
     void testExample() {
 
-        JsonPath response = RestAssured
-                .given()
-                .get("https://playground.learnqa.ru/api/get_json_homework")
-                .jsonPath();
 
-       // response.prettyPrint();
-        String answer = response.get("");
-        System.out.println(answer);
+        Response response = RestAssured
+               .given()
+               .get("https://playground.learnqa.ru/api/get_json_homework")
+                .andReturn();
+
+//print(json_text['messages'][0])
+       //String answer = response.prettyPrint();
+
+        response.print();
+
+    }
+
+
+    @Test
+    void ex6Test() {
+        Response response = RestAssured
+                .given()
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+    Header responseHeaders = response.getHeaders().get("X-Host");
+    System.out.println(responseHeaders);
+
     }
 }

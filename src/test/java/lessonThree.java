539ae2ff3c371a0448841875c29aa4d41af23423
Andrey Zavrichko <andrey@zavrichko.dev>
2022-09-24 13:07:13 +0300

    добавил тест ex10Test

--- src/test/java/lessonThree.java
+++ src/test/java/lessonThree.java
@@ -1,9 +1,20 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class lessonThree {
-    @Test
-    void ex10Test() {
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
+class lessonThree {
+    @ParameterizedTest
+    @ValueSource(strings = {"123456789012345", "1234567890123456", "12345"})
+    void ex10Test(String inputValue) {
+        assertTrue(inputValue.length() >= 15, "Значение inputValue больше или равно 15: " + inputValue);
     }
 }
+
+
+
+
+
+
+

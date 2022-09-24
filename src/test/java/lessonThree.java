7dbdfa2120c3cd99eff61a2b093a14ed51bb608c
Andrey Zavrichko <andrey@zavrichko.dev>
2022-09-24 13:27:23 +0300

    добавил тест ex11Test

--- src/test/java/lessonThree.java
+++ src/test/java/lessonThree.java
@@ -1,6 +1,10 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -9,6 +13,18 @@ class lessonThree {
     @ValueSource(strings = {"123456789012345", "1234567890123456", "12345"})
     void ex10Test(String inputValue) {
         assertTrue(inputValue.length() >= 15, "Значение inputValue больше или равно 15: " + inputValue);
+    }
+
+    @Test
+    void ex11Test(){
+        Response response = RestAssured
+                .given()
+                .log().all()
+                .when()
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        assertEquals("hw_value", response.getCookie("HomeWork"));
+        System.out.println(response.getCookies());
     }
 }
 

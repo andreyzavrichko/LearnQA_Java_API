91f1fe2ac4ec0f9ed3338dfe92411fdc8439d5ab
Andrey Zavrichko <andrey@zavrichko.dev>
2022-10-15 10:45:35 +0300

    добавил Ex16: Запрос данных другого пользователя

---
+++ src/test/java/tests/UserGetTest.java
@@ -0,0 +1,59 @@
+package tests;
+
+import io.qameta.allure.Epic;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.ApiCoreRequests;
+import lib.Assertions;
+import lib.BaseTestCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@DisplayName("Получение данных пользователя")
+class UserGetTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
+    @Test
+    @Epic("Получение данных")
+    @DisplayName("Получение данных неавторизованным пользователем")
+    void testGetUserDataNotAuth() {
+        Response responseUserData = RestAssured
+                .get("https://playground.learnqa.ru/api/user/2")
+                .andReturn();
+
+        String[] unexpectedFieldNames = {"firstName", "lastName", "email"};
+        Assertions.assertJsonHasField(responseUserData, "username");
+        Assertions.assertJsonHasNotFields(responseUserData, unexpectedFieldNames);
+    }
+
+    @Test
+    @Epic("Получение данных")
+    @DisplayName("Получение данных другого пользователя авторизованным пользователем")
+    void testGetUserDataAuthAsSomeUser() {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+        Response responseGetAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+
+        Response responseUserData = RestAssured
+                .given()
+                .log().all()
+                .header("x-csrf-token", header)
+                .cookie("auth_sid", cookie)
+                .when()
+                .get("https://playground.learnqa.ru/api/user/1")
+                .andReturn();
+        responseUserData.prettyPrint();
+        String[] expectedFieldNames = {"username"};
+        Assertions.assertJsonHasFields(responseUserData, expectedFieldNames);
+    }
+
+}
